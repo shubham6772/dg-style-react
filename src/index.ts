32ee330dea5b7268d688cd45a5a714ac9ceb61bd
Shubham Verma <shubhamv@izooto.com>
2024-07-21 14:27:54 +0530

    @dgsr--Created ProgressBar and also Improve the fileStructure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export {default as ColorSpinner} from "./components/Spinner/ColorSpinner/ColorSpinner";
+export {default as ColorSpinner} from "./components/Loaders/Spinner/ColorSpinner/ColorSpinner";
+export {default as ThreeSpinner} from "./components/Loaders/Spinner/ThreeSpinner/ThreeSpinner";
+export {default as ProgressBar} from "./components/Loaders/ProgressBar/ProgressBar";
